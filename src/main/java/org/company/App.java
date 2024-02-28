9b58aa90584d5ede93772af845248bb709e422fd
Badman1332 <mik.davidsson@gmail.com>
2024-02-28 16:56:18 +0100

    Initial commit

---
+++ src/main/java/org/company/App.java
@@ -0,0 +1,13 @@
+package org.company;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello!" );
+        System.out.println( "My name is" );
+        System.out.println( "Mikael Davidsson." );
+    }
+
+}
